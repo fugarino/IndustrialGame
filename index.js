ccb54380f7fe852c193b5c37e05c5f660082cf9b
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-05-30 10:24:51 -0400

    import and rendered map

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 // Selectors
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
-console.log(c);
 
-c.fillStyle = "lightBlue";
+canvas.width = 3024;
+canvas.height = 2076;
+
+c.fillStyle = "rgb(20, 20, 20)";
 c.fillRect(0, 0, canvas.width, canvas.height);
+
+const mapImage = new Image();
+mapImage.src = "./img/IndustrialGameTilesetMap.png";
+
+console.log(mapImage);
+
+mapImage.onload = () => {
+  c.drawImage(mapImage, -550, -100);
+};
