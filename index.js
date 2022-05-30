56559dfaa0035fa8a93f8e482d05a4eaad2a36ab
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-05-30 11:40:48 -0400

    character placement

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 
-canvas.width = 3024;
-canvas.height = 2076;
+// canvas.width = 3024;
+// canvas.height = 2076;
+canvas.width = 3600;
+canvas.height = 2160;
 
 c.fillStyle = "rgb(20, 20, 20)";
 c.fillRect(0, 0, canvas.width, canvas.height);
@@ -11,8 +13,21 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 const mapImage = new Image();
 mapImage.src = "./img/IndustrialGameTilesetMap.png";
 
-console.log(mapImage);
+const playerImage = new Image();
+playerImage.src = "./img/IdleRight.png";
 
 mapImage.onload = () => {
-  c.drawImage(mapImage, -550, -100);
+  // c.drawImage(mapImage, -550, -100);
+  c.drawImage(mapImage, 60, 0);
+  c.drawImage(
+    playerImage,
+    0,
+    0,
+    playerImage.width / 8,
+    playerImage.height,
+    canvas.width / 2 - playerImage.width / 4 / 2,
+    canvas.height / 2 - playerImage.height / 2,
+    playerImage.width / 8,
+    playerImage.height
+  );
 };
